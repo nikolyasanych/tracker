c48fd024127d81d2a77c06b235ec3686bfe9b1cb
nikolyasanych <Shaposhnikovnick@yandex.ru>
2023-11-29 20:52:56 +0500

    7.2

---
+++ src/main/java/ru/oop/Dog.java
@@ -0,0 +1,9 @@
+package ru.oop;
+
+public class Dog {
+    public static void main(String[] args) {
+        Dog polkan = new Dog();
+        Dog petrovich = new Dog();
+        Dog jora = new Dog();
+    }
+}
