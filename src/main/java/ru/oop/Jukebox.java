b54402b9013972025b1460a309248ed49bac979e
nikolyasanych <Shaposhnikovnick@yandex.ru>
2023-12-04 11:24:48 +0500

    7.4 Вызов метода с аргументами ДЗ

---
+++ src/main/java/ru/oop/Jukebox.java
@@ -0,0 +1,20 @@
+package ru.oop;
+
+public class Jukebox {
+    public void music(int position) {
+        if (position == 1) {
+            System.out.println("Пусть бегут неуклюже");
+        } else if (position == 2) {
+            System.out.println("Спокойной ночи");
+        } else {
+            System.out.println("Песня не найдена");
+        }
+    }
+
+    public static void main(String[] args) {
+        Jukebox vanya = new Jukebox();
+        vanya.music(1);
+        vanya.music(2);
+        vanya.music(3);
+    }
+}
