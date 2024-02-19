787dad2b1be64162e9b7d906c09b9968b6121bbe
nikolyasanych <Shaposhnikovnick@yandex.ru>
2024-02-19 15:57:41 +0500

    9.6 Сравнение моделей. Метод equals ДЗ

--- src/main/java/ru/pojo/Product.java
+++ src/main/java/ru/pojo/Product.java
@@ -1,5 +1,7 @@
 package ru.pojo;
 
+import java.util.Objects;
+
 public class Product {
     private String name;
     private int count;
@@ -25,4 +27,20 @@ public class Product {
         this.count = count;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Product product = (Product) o;
+        return count == product.count && Objects.equals(name, product.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, count);
+    }
 }
