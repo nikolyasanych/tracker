133a0d8465810f319678d178116f833c20ba81b3
nikolyasanych <Shaposhnikovnick@yandex.ru>
2024-02-19 16:50:05 +0500

    9.7 Tracker - хранилище ДЗ

--- src/main/java/ru/tracker/Tracker.java
+++ src/main/java/ru/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -9,6 +11,32 @@ public class Tracker {
         item.setId(ids++);
         items[size++] = item;
         return item;
+    }
+
+    public Item[] findAll() {
+        int len = 0;
+        Item[] rsl = new Item[size];
+        for (int i = 0; i < size; i++) {
+            Item name = items[i];
+            if (name != null) {
+                rsl[len] = name;
+                len++;
+            }
+        }
+        return Arrays.copyOf(rsl, len);
+    }
+
+    public Item[] findByName(String key) {
+        int len = 0;
+        Item[] rsl = new Item[size];
+        for (int i = 0; i < size; i++) {
+            Item name = items[i];
+            if (name.getName().equals(key)) {
+                rsl[len] = name;
+                len++;
+            }
+        }
+        return Arrays.copyOf(rsl, len);
     }
 
     public Item findById(int id) {
