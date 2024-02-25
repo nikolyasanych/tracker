c939d97cf79568f3edf8cde0954fb2acc43771d1
nikolyasanych <Shaposhnikovnick@yandex.ru>
2024-02-25 17:02:33 +0500

    9.15

---
+++ src/main/java/ru/concat/JoinExample.java
@@ -0,0 +1,8 @@
+package ru.concat;
+
+public class JoinExample {
+    public static void main(String[] args) {
+        String str = String.join(" ", "abc", "def", "ghi");
+        System.out.println(str);
+    }
+}
