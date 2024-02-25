f6c1b16140839db757b518efc4c4dbe015122af3
nikolyasanych <Shaposhnikovnick@yandex.ru>
2024-02-25 17:23:55 +0500

    9.16

---
+++ src/main/java/ru/varargs/VarArgs.java
@@ -0,0 +1,18 @@
+package ru.varargs;
+
+public class VarArgs {
+    public static String text(String... strings) {
+        StringBuilder builder = new StringBuilder();
+        for (String s : strings) {
+            builder.append(s);
+        }
+        return builder + " Количество соединенных элементов: " + strings.length;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(text());
+        System.out.println(text("aaa"));
+        System.out.println(text("aaa", "bbb", "ccc"));
+        System.out.println(text("aaa", "bbb", "ccc", "ddd", "eee"));
+    }
+}
