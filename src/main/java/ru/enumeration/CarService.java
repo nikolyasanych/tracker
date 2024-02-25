8b6f48f95513e600f4b2eaa001a64b65aed3691b
nikolyasanych <Shaposhnikovnick@yandex.ru>
2024-02-25 16:25:35 +0500

    9.14

---
+++ src/main/java/ru/enumeration/CarService.java
@@ -0,0 +1,17 @@
+package ru.enumeration;
+
+public class CarService {
+    public static void main(String[] args) {
+        Status toyota = Status.FINISHED;
+        Status volvo = Status.WAITING;
+        System.out.println("Статус Toyota: " + toyota);
+        System.out.println("Статус Volvo: " + volvo);
+        Status[] statuses = Status.values();
+        for (Status s : statuses) {
+            System.out.println("Название статуса: " + s.name() + ", Порядковый номер статуса: " + s.ordinal());
+        }
+        String accepted = "ACCEPTED";
+        Status status = Status.valueOf(accepted);
+        System.out.println(status);
+    }
+}
