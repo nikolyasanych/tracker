96380ff66080f703820089173cf1e98e0e54b02e
nikolyasanych <Shaposhnikovnick@yandex.ru>
2024-02-27 23:28:56 +0500

    10.4

--- src/main/java/ru/tracker/StartUI.java
+++ src/main/java/ru/tracker/StartUI.java
@@ -1,13 +1,38 @@
 package ru.tracker;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
 public class StartUI {
-    public static void main(String[] args) {
-        Item item = new Item();
-        LocalDateTime rsl = item.getCreated();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
-        String data = rsl.format(formatter);
-        System.out.println(data);
+    public void init(Scanner scanner, Tracker tracker) {
+        boolean run = true;
+        while (run) {
+            showMenu();
+            System.out.print("Выбрать: ");
+            int select = Integer.parseInt(scanner.nextLine());
+            if (select != 6) {
+                System.out.println("Пользователь выбрал: " + select);
+            } else {
+                run = false;
+            }
+        }
     }
+
+    private void showMenu() {
+        String[] menu = {
+                "Добавить новую заявку", "Показать все заявки", "Изменить заявку",
+                "Удалить заявку", "Показать заявку по id", "Показать заявки по имени",
+                "Завершить программу"
+        };
+        System.out.println("Меню:");
+        for (int i = 0; i < menu.length; i++) {
+            System.out.println(i + ". " + menu[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Tracker tracker = new Tracker();
+        new StartUI().init(scanner, tracker);
+    }
+
 }
