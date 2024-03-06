e2fc9738befe6a47b6daa883cde7247d40b0c358
nikolyasanych <Shaposhnikovnick@yandex.ru>
2024-03-06 18:48:39 +0500

    10.14

---
+++ src/main/java/ru/oop/College.java
@@ -0,0 +1,9 @@
+package ru.oop;
+
+public class College {
+    public static void main(String[] args) {
+        Freshman fr = new Freshman();
+        Student student = fr;
+        Object obj = student;
+    }
+}
