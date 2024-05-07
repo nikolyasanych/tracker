62a943fdfb3909c2e3da6bf47b37d624822a60b4
nikolyasanych <Shaposhnikovnick@yandex.ru>
2024-05-07 23:27:00 +0500

    13.4

--- src/test/java/ru/search/PhoneDictionaryTest.java
+++ src/test/java/ru/search/PhoneDictionaryTest.java
@@ -16,4 +16,14 @@ public class PhoneDictionaryTest {
         ArrayList<Person> persons = phones.find("Ivan");
         assertThat(persons.get(0).getSurname()).isEqualTo("Ivanov");
     }
+
+    @Test
+    public void whenNoFoundByName() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Ivan", "Ivanov", "534872", "Bryansk")
+        );
+        ArrayList<Person> persons = phones.find("Kolya");
+        assertThat(persons.isEmpty()).isTrue();
+    }
 }
