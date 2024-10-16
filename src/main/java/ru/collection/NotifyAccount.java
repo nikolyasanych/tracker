a9b9331fbd2565eff9eb53bf28e6b39f1cbeaa07
nikolyasanych <Shaposhnikovnick@yandex.ru>
2024-10-16 19:59:19 +0500

    14.4 модели данных и HashSet ДЗ

---
+++ src/main/java/ru/collection/NotifyAccount.java
@@ -0,0 +1,14 @@
+package ru.collection;
+
+import java.util.HashSet;
+import java.util.List;
+
+public class NotifyAccount {
+    public static HashSet<Account> sent(List<Account> accounts) {
+        HashSet<Account> result = new HashSet<>();
+        for (Account account : accounts) {
+            result.add(account);
+        }
+        return result;
+    }
+}
