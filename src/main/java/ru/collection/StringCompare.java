8f81e8caabddf65c1ff9c57752c301b929bda4aa
nikolyasanych <Shaposhnikovnick@yandex.ru>
2024-11-06 18:34:55 +0500

    17.4

--- src/main/java/ru/collection/StringCompare.java
+++ src/main/java/ru/collection/StringCompare.java
@@ -5,19 +5,16 @@ import java.util.Comparator;
 public class StringCompare implements Comparator<String> {
     @Override
     public int compare(String left, String right) {
-        int leftSize = left.length();
-        int rightSize = right.length();
-        int minSize = Math.min(leftSize, rightSize);
-        int compareSize = Integer.compare(leftSize, rightSize);
+        int minSize = Math.min(left.length(), right.length());
         for (int i = 0; i < minSize; i++) {
             int result = Character.compare(left.charAt(i), right.charAt(i));
             if (result != 0) {
                 return result;
             }
-            if (compareSize < 0 && i == minSize - 1) {
+            if (left.length() < right.length() && i == minSize - 1) {
                 return -1;
             }
-            if (compareSize > 0 && i == minSize - 1) {
+            if (left.length() > right.length() && i == minSize - 1) {
                 return 1;
             }
         }
