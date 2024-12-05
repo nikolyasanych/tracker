ef019b874a1a0f16c4fd84e8fd95ab4afeef9a0c
nikolyasanych <Shaposhnikovnick@yandex.ru>
2024-12-05 23:01:53 +0500

    20.8

---
+++ src/main/java/ru/lambda/Model.java
@@ -0,0 +1,16 @@
+package ru.lambda;
+
+public class Model {
+    private String name;
+
+    Model() {
+    }
+
+    Model(String str) {
+        name = str;
+    }
+
+    String getName() {
+        return name;
+    }
+}
