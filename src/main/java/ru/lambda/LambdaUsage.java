1f880b13ce6351d1a8e6928dd98d3e54b1307a3c
nikolyasanych <Shaposhnikovnick@yandex.ru>
2024-12-05 22:48:38 +0500

    20.3

---
+++ src/main/java/ru/lambda/LambdaUsage.java
@@ -0,0 +1,16 @@
+package ru.lambda;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class LambdaUsage {
+
+    public static void main(String[] args) {
+        List<String> strings = Arrays.asList("eeeee", "a", "ccc", "dddd", "bb");
+        Comparator<String> comparator = (left, right) -> {
+            System.out.println("compare - " + left.length() + " : " + right.length());
+            return Integer.compare(right.length(), left.length());
+        };
+    }
+}
