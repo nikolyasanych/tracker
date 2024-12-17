9ce8e2c6bcb67c30cc20f2642dcf0d2da9ee0d70
nikolyasanych <Shaposhnikovnick@yandex.ru>
2024-12-17 22:30:00 +0500

    20.8

---
+++ src/test/java/ru/function/FunctionCalculatorTest.java
@@ -0,0 +1,34 @@
+package ru.function;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class FunctionCalculatorTest {
+    @Test
+    void whenLinearFunctionThenLinearResults() {
+        FunctionCalculator function = new FunctionCalculator();
+        List<Double> result = function.diapason(5, 8, x -> 2 * x + 1);
+        List<Double> expected = Arrays.asList(11D, 13D, 15D);
+        assertThat(result).containsAll(expected);
+    }
+
+    @Test
+    void whenQuadraticFunctionThenQuadraticResults() {
+        FunctionCalculator function = new FunctionCalculator();
+        List<Double> result = function.diapason(0, 3, x -> 5 * Math.pow(x, 2));
+        List<Double> expected = Arrays.asList(0D, 5D, 20D);
+        assertThat(result).containsAll(expected);
+    }
+
+    @Test
+    void whenIndicativeFunctionThenIndicativeResults() {
+        FunctionCalculator function = new FunctionCalculator();
+        List<Double> result = function.diapason(0, 5, x -> Math.pow(2, x));
+        List<Double> expected = Arrays.asList(1D, 2D, 4D, 8D, 16D);
+        assertThat(result).containsAll(expected);
+    }
+}
