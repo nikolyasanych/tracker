ffdaa9fb5e00d8c081b41acce53e23d72456ecd3
nikolyasanych <Shaposhnikovnick@yandex.ru>
2025-01-30 22:50:01 +0500

    21.7

--- src/main/java/ru/stream/Profiles.java
+++ src/main/java/ru/stream/Profiles.java
@@ -9,4 +9,12 @@ public class Profiles {
                 .map(profile -> profile.getAddress())
                 .toList();
     }
+
+    public static List<Address> collectSortWithoutDuplicate(List<Profile> profiles) {
+        return profiles.stream()
+                .map(profile -> profile.getAddress())
+                .distinct()
+                .sorted((left, right) -> left.getCity().compareTo(right.getCity()))
+                .toList();
+    }
 }
