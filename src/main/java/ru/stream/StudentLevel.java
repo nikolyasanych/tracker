65b7a444e4f5df53fcc2b9ef32636c204965b244
nikolyasanych <Shaposhnikovnick@yandex.ru>
2025-05-31 18:23:32 +0500

    22.1 Stream API улучшения

---
+++ src/main/java/ru/stream/StudentLevel.java
@@ -0,0 +1,15 @@
+package ru.stream;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+public class StudentLevel {
+    public static List<Student> levelOf(List<Student> students, int bound) {
+        return students.stream()
+                .flatMap(Stream::ofNullable)
+                .sorted((left, right) -> Integer.compare(right.getScore(), left.getScore()))
+                .takeWhile(value -> value.getScore() >= bound)
+                .toList();
+    }
+
+}
